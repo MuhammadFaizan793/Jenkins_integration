176a28c60a17bfc182b3a1cd8356bb252ecc5ba1
Muhammad Faizan <110288755+MuhammadFaizan793@users.noreply.github.com>
2025-08-15 14:31:40 -0700

    TestNG Files

---
+++ src/test/java/seleniumpkg/Test2.java
@@ -0,0 +1,46 @@
+package seleniumpkg;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.time.Duration;
+
+public class Test2 {
+
+    @Test
+    public void GoogleSearchTest() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.findElement(By.name("q")).sendKeys("CG Technologies", Keys.ENTER);
+        Assert.assertTrue(driver.getTitle().contains("CG Technologies"), "Title does not contain search term");
+        driver.quit();
+    }
+
+    @Test
+    public void FacebookLoginPageTest() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://facebook.com");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        Assert.assertTrue(driver.findElement(By.name("login")).isDisplayed(), "Login button not visible");
+        driver.quit();
+    }
+
+    @Test
+    public void YouTubeSearchTest() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://youtube.com");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.findElement(By.name("search_query")).sendKeys("Selenium Automation", Keys.ENTER);
+//        Assert.assertTrue(driver.getTitle().contains("Selenium Automation"), "Search did not work as expected");
+        driver.quit();
+    }
+}
