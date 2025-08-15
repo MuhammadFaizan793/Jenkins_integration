abb41ea8d4f7ff97308208e256940075a1cc7ce5
Muhammad Faizan <110288755+MuhammadFaizan793@users.noreply.github.com>
2025-08-15 14:46:12 -0700

    Delete src directory

--- src/test/java/seleniumpkg/Path.java
+++
@@ -1,6 +0,0 @@
-package seleniumpkg;
-
-public class Path {
-    public static final String FACEBOOK_URL = "https://facebook.com";
-//    public static final String CHROME_DRIVER_PATH = "C:\\browserdrivers\\chromedriver.exe";
-}
