49ce04133b148a0c044f0c0ea9151e603499afdc
Muhammad Faizan <110288755+MuhammadFaizan793@users.noreply.github.com>
2025-08-15 15:26:31 -0700

    Update FacebookLoginTest.java

--- src/test/java/seleniumpkg/FacebookLoginTest.java
+++ src/test/java/seleniumpkg/FacebookLoginTest.java
@@ -3,8 +3,8 @@ package seleniumpkg;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class FacebookLoginTest {
@@ -13,20 +13,21 @@ public class FacebookLoginTest {
 
     @BeforeMethod
     public void setUp() {
-        driver = new ChromeDriver();
-        driver.get("https://facebook.com");
-        driver.manage().window().maximize();
+        driver = new ChromeDriver(); // no shadowing
+        Snippet.openUrl(driver, Path.FACEBOOK_URL);
     }
 
     @Test
-    public void loginTest() {
-        driver.findElement(By.id("email")).sendKeys("testuser");
-        driver.findElement(By.id("pass")).sendKeys("password");
-        driver.findElement(By.name("login")).click();
+    public void testLogin() {
+        driver.findElement(By.id("email")).sendKeys("test@gmail.com");
+        driver.findElement(By.id("pass")).sendKeys("test@gmail.com");
+        driver.findElement(By.xpath("//button[contains(@class,'_6lth')]")).click();
     }
 
     @AfterMethod
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
