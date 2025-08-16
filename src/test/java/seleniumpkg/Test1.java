4dd10b9221c108ad1db0048694f64895712363ea
Muhammad Faizan <110288755+MuhammadFaizan793@users.noreply.github.com>
2025-08-15 23:17:50 -0700

    Update Test1.java

--- src/test/java/seleniumpkg/Test1.java
+++ src/test/java/seleniumpkg/Test1.java
@@ -20,9 +20,10 @@ public class Test1 {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(500));
 		driver.findElement(By.name("q")).sendKeys("CG Technologies", Keys.ENTER);
-		System.out.print(driver.getTitle());
+		// System.out.print(driver.getTitle());
 		
 
 	}
 
 }
+
