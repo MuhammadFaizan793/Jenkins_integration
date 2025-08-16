e44eedbbe0300ee6678aed4d3048b1671ed6d83e
Muhammad Faizan <110288755+MuhammadFaizan793@users.noreply.github.com>
2025-08-15 23:26:02 -0700

    Update FacebookLoginTest.java

--- src/test/java/seleniumpkg/FacebookLoginTest.java
+++ src/test/java/seleniumpkg/FacebookLoginTest.java
@@ -1,36 +1,37 @@
-package seleniumpkg;
+// package seleniumpkg;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.testng.annotations.AfterMethod;
+// import org.testng.annotations.BeforeMethod;
+// import org.testng.annotations.Test;
 
-public class FacebookLoginTest {
+// public class FacebookLoginTest {
 
-    WebDriver driver;
+//     WebDriver driver;
 
-    @BeforeMethod
-    public void setUp() {
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://facebook.com");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    }
+//     @BeforeMethod
+//     public void setUp() {
+//         ChromeDriver driver = new ChromeDriver();
+//         driver.get("https://facebook.com");
+//         driver.manage().window().maximize();
+//         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//     }
 
-    @Test
-    public void testLogin() {
-        driver.findElement(By.id("email")).sendKeys("test@gmail.com");
-        driver.findElement(By.id("pass")).sendKeys("test@gmail.com");
-        driver.findElement(By.xpath("//button[contains(@class,'_6lth')]")).click();
-    }
+//     @Test
+//     public void testLogin() {
+//         driver.findElement(By.id("email")).sendKeys("test@gmail.com");
+//         driver.findElement(By.id("pass")).sendKeys("test@gmail.com");
+//         driver.findElement(By.xpath("//button[contains(@class,'_6lth')]")).click();
+//     }
 
-    @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-}
+//     @AfterMethod
+//     public void tearDown() {
+//         if (driver != null) {
+//             driver.quit();
+//         }
+//     }
+// }
+
 
